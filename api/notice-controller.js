4e0f8f421ea300785406e4023f41477dc8047105
jiangbo <jiangbo@uniugame.com>
2018-02-01 11:29:18 +0800

    *更新公告代码

---
+++ api/notice-controller.js
@@ -0,0 +1,84 @@
+var fs = require("fs");
+var formidable = require("formidable");
+var path = require('path');
+var md5 = require('MD5');
+var dataFormat = require('dataformat');
+var bodyParser = require('body-parser');
+var config = require('../config');
+var images = require("images");
+
+var getnoticecontent = function(req,res)
+{
+    var textureMD5 = req.body.md5;
+    console.log(textureMD5);
+    
+    var save_path = config.upload_notice + "/notice.txt";
+    console.log(save_path);
+    fs.exists(save_path,function(exists)
+    {
+        if(exists)//存在，读取
+        {
+            fs.readFile(save_path,'utf8',function(err,data)
+            {
+                if(err)
+                {
+                    saveResult(res,"none");
+                }
+                else
+                {
+                    saveResult(res,data);
+                }
+            });
+        }
+        else //不存在，创建
+        {
+            saveResult(res,"none");
+        }
+    });
+};
+
+var getnoticeimage = function(req,res)
+{
+    var textureMD5 = req.body.md5;
+    console.log(textureMD5);
+    
+    var save_path = config.upload_notice + "/notice.png";
+    console.log(save_path);
+    fs.exists(save_path,function(exists)
+    {
+        if(exists)//存在，读取
+        {
+            fs.exists(save_path,function(exists)
+            {
+                if(exists)
+                {
+                    fs.readFile(save_path,function(err,data)
+                    {
+                        res.setHeader("Content-Type", "image/png");
+                        res.writeHead(200, "Ok");
+                        res.write(data,"binary"); //格式必须为 binary，否则会出错
+                        console.log("下载舞团团徽成功");
+                        res.end();
+                    });
+                }
+                else
+                {
+                    saveResult(res,'false');
+                }
+            });
+        }
+        else //不存在，创建
+        {
+            saveResult(res,"none");
+        }
+    });
+};
+
+function saveResult(res,data)
+{
+	res.writeHead(200);
+	res.write(data);
+	res.end();
+}
+exports.getnoticecontent=getnoticecontent;
+exports.getnoticeimage=getnoticeimage;
